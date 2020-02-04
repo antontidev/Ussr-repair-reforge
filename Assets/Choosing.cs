1c25cfbc4d6f90225c35150dc1d667b074a028dd
pink production <sanjaplushka@gmail.com>
2020-02-04 23:37:15 +0500

    initial commit

---
+++ Assets/Choosing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choosing : MonoBehaviour
+{
+    public Material material;
+    private MeshRenderer meshRenderer;
+    public Material defaultMaterial;
+    private void OnMouseEnter()
+    {
+        meshRenderer.material = material;
+    }
+
+    private void OnMouseExit()
+    {
+        meshRenderer.material = defaultMaterial;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/OrbitMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMovement : MonoBehaviour
+{
+    public float speed;
+    public float smoothingFactor;
+
+    public Transform objectToMove;
+    public Vector3 pointWithOrbit;
+
+    private Vector3 viewOffset;
+    private Vector3 cameraOffset;
+    private Vector3 startNewMiddle;
+    private Vector3 endNewMiddle;
+
+    private bool changingMiddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - objectToMove.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            changingMiddle = true;
+            startNewMiddle = Camera.main.ScreenToWorldPoint(transform.position);
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            changingMiddle = false;
+            endNewMiddle = Camera.main.ScreenToWorldPoint(transform.position);
+            //или наоборот
+            viewOffset = endNewMiddle - startNewMiddle;
+        }
+    }
+    void LateUpdate()
+    {
+        if (!changingMiddle)
+        {
+            Quaternion camTurnAngle = Quaternion.Euler(Input.GetAxis("Mouse X") * speed, Input.GetAxis("Mouse Y") * speed, 0);
+
+
+            cameraOffset = camTurnAngle * cameraOffset;
+
+            Vector3 newPos = objectToMove.position + cameraOffset;
+
+            transform.position = Vector3.Slerp(transform.position, newPos, Time.deltaTime * smoothingFactor);
+
+            transform.LookAt(objectToMove.transform.position + viewOffset);
+        }
+    }
+}
